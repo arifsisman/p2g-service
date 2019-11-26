ce7cad52f793a16436c16828d48835c1a01d1800
Mustafa Arif Sisman <mustafaarifsisman@gmail.com>
2019-11-26 13:44:01 +0300

    impl: RoleService changeRole method implemented.

--- src/main/java/vip/yazilim/p2g/web/service/impl/RoleService.java
+++ src/main/java/vip/yazilim/p2g/web/service/impl/RoleService.java
@@ -13,6 +13,7 @@ import vip.yazilim.p2g.web.repository.IRoleRepo;
 import vip.yazilim.p2g.web.service.IRoleService;
 import vip.yazilim.p2g.web.service.relation.IRoomUserService;
 import vip.yazilim.spring.utils.exception.DatabaseException;
+import vip.yazilim.spring.utils.exception.InvalidUpdateException;
 import vip.yazilim.spring.utils.service.ACrudServiceImpl;
 
 import java.util.Optional;
@@ -56,6 +57,50 @@ public class RoleService extends ACrudServiceImpl<Role, String> implements IRole
         return getById(roleName);
     }
 
+    @Override
+    public String changeUserRole(String roomUuid, String userUuid, boolean rank) throws DatabaseException, RoleException {
+        Optional<RoomUser> roomUserOpt = roomUserService.getRoomUser(roomUuid, userUuid);
+        RoomUser roomUser;
+
+        if (!roomUserOpt.isPresent()) {
+            LOGGER.warn("User[{}] not in Room[{}]", userUuid, roomUuid);
+            return getDefaultRole().getName();
+        }else{
+            roomUser = roomUserOpt.get();
+        }
+
+        String roleName = roomUser.getRoleName();
+
+        if (rank) {
+            switch (roleName) {
+                case "USER":
+                    roleName = Roles.MODERATOR.getRoleName();
+                    break;
+                case "MODERATOR":
+                    roleName = Roles.ADMIN.getRoleName();
+                    break;
+            }
+        } else {
+            switch (roleName) {
+                case "MODERATOR":
+                    roleName = Roles.USER.getRoleName();
+                    break;
+                case "ADMIN":
+                    roleName = Roles.MODERATOR.getRoleName();
+                    break;
+            }
+        }
+
+        roomUser.setRoleName(roleName);
+
+        try {
+            roomUserService.update(roomUser);
+        } catch (InvalidUpdateException e) {
+            e.printStackTrace();
+        }
+
+        return roleName;
+    }
 
     @Override
     public Role getDefaultRole() throws DatabaseException, RoleException {
