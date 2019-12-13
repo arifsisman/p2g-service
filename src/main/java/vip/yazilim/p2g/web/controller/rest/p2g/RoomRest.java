122066723b0681591ff100779e6b3dd91830e0f5
Mustafa Arif Şişman <mustafaarifsisman@gmail.com>
2019-12-14 00:36:39 +0300

    impl(WIP): RoomInviteRest and RoomUserRest endpoints merged in RoomRest impl(WIP): Missing joinRoom endpoint

--- src/main/java/vip/yazilim/p2g/web/controller/rest/p2g/RoomRest.java
+++ src/main/java/vip/yazilim/p2g/web/controller/rest/p2g/RoomRest.java
@@ -1,18 +1,19 @@
 package vip.yazilim.p2g.web.controller.rest.p2g;
 
-import io.swagger.annotations.ApiResponse;
-import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import vip.yazilim.p2g.web.entity.Room;
+import vip.yazilim.p2g.web.entity.relation.RoomInvite;
+import vip.yazilim.p2g.web.entity.relation.RoomUser;
 import vip.yazilim.p2g.web.model.RoomModel;
 import vip.yazilim.p2g.web.service.p2g.IRoomService;
+import vip.yazilim.p2g.web.service.p2g.relation.IRoomInviteService;
+import vip.yazilim.p2g.web.service.p2g.relation.IRoomUserService;
 import vip.yazilim.spring.core.exception.web.NotFoundException;
 import vip.yazilim.spring.core.exception.web.ServiceException;
 import vip.yazilim.spring.core.rest.ARestCrud;
-import vip.yazilim.spring.core.rest.model.RestErrorResponse;
 import vip.yazilim.spring.core.rest.model.RestResponse;
 import vip.yazilim.spring.core.service.ICrudService;
 import vip.yazilim.spring.core.util.RestResponseFactory;
@@ -34,15 +35,20 @@ public class RoomRest extends ARestCrud<Room, String> {
     @Autowired
     private IRoomService roomService;
 
+    @Autowired
+    private IRoomUserService roomUserService;
+
+    @Autowired
+    private IRoomInviteService roomInviteService;
+
     @Override
     protected ICrudService<Room, String> getService() {
         return roomService;
     }
 
+    // Room
     @PreAuthorize(value = "hasAuthority('join_room')")
     @GetMapping("/{roomUuid}/model")
-    @CrossOrigin(origins = {"*"})
-    @ApiResponses({@ApiResponse(code = 404, message = "Model not found", response = RestErrorResponse.class), @ApiResponse(code = 500, message = "Internal Error", response = RestErrorResponse.class)})
     public RestResponse<RoomModel> getRoomModel(HttpServletRequest request, HttpServletResponse response, @PathVariable String roomUuid) {
         Optional<RoomModel> roomModel;
 
@@ -61,8 +67,6 @@ public class RoomRest extends ARestCrud<Room, String> {
 
     @Override
     @DeleteMapping("/{roomUuid}")
-    @CrossOrigin(origins = {"*"})
-    @ApiResponses({@ApiResponse(code = 404, message = "Entity not found", response = RestErrorResponse.class), @ApiResponse(code = 500, message = "Internal Error", response = RestErrorResponse.class)})
     public RestResponse<Boolean> delete(HttpServletRequest request, HttpServletResponse response, @PathVariable String roomUuid) {
         boolean status;
 
@@ -75,4 +79,50 @@ public class RoomRest extends ARestCrud<Room, String> {
 
         return RestResponseFactory.generateResponse(status, HttpStatus.OK, request, response);
     }
+
+    // RoomInvite
+    @PostMapping("/{roomUuid}/invite/{userUuid}")
+    public RestResponse<RoomUser> invite(HttpServletRequest request, HttpServletResponse response, @PathVariable String roomUuid, @PathVariable String userUuid) {
+        RoomUser roomUser = new RoomUser();
+
+        roomUser.setRoomUuid(roomUuid);
+        roomUser.setUserUuid(userUuid);
+
+        try {
+            roomUser = roomUserService.create(roomUser);
+        } catch (Exception e) {
+            throw new ServiceException(e);
+        }
+
+        return RestResponseFactory.generateResponse(roomUser, HttpStatus.OK, request, response);
+    }
+
+    @PostMapping("/invite")
+    public RestResponse<RoomUser> accept(HttpServletRequest request, HttpServletResponse response, @RequestBody RoomInvite roomInvite) {
+        RoomUser roomUser;
+
+        try {
+            roomUser = roomUserService.acceptRoomInvite(roomInvite);
+        } catch (Exception e) {
+            throw new ServiceException(e);
+        }
+
+        return RestResponseFactory.generateResponse(roomUser, HttpStatus.OK, request, response);
+    }
+
+    @DeleteMapping("/invite/{roomInviteUuid}")
+    public RestResponse<Boolean> reject(HttpServletRequest request, HttpServletResponse response, @PathVariable String roomInviteUuid) {
+        boolean status;
+
+        try {
+            status = roomInviteService.reject(roomInviteUuid);
+        } catch (Exception e) {
+            throw new ServiceException(e);
+        }
+
+        return RestResponseFactory.generateResponse(status, HttpStatus.OK, request, response);
+    }
+
+    // RoomUser
+
 }
