f96110e7d32a427146bd15b8d1a11c6a20be591f
Mustafa Arif Şişman <mustafaarifsisman@gmail.com>
2020-01-16 05:51:44 +0300

    gitlab-ci.yml updated

--- docker-init.sh
+++ docker-init.sh
@@ -12,5 +12,3 @@
 #docker build -t ${IMAGE_NAME} --build-arg ARTIFACT_NAME=${ARTIFACT_NAME} --no-cache .
 #docker rm -vf ${CONTAINER_NAME}
 #docker run -d --name ${CONTAINER_NAME} -p 8080:8080 ${IMAGE_NAME}
-
-mvn org.apache.maven.plugins:maven-help-plugin:2.1.1:evaluate -Dexpression=project.artifactId | grep -ov "INFO" | grep -ov "Down" | grep -ov "Prog"
