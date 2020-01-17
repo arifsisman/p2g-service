b1b3c7481a461a58efa3af93288462966cdd0bfe
Mustafa Arif Şişman <mustafaarifsisman@gmail.com>
2020-01-17 06:22:45 +0300

    gitlab-ci.yml updated

--- test.sh
+++ test.sh
@@ -1,11 +1,11 @@
 ARTIFACT_ID_SCRIPT="mvn org.apache.maven.plugins:maven-help-plugin:2.1.1:evaluate -Dexpression=project.artifactId | grep -Eiv 'INFO|Down|Prog'"
 POM_VERSION_SCRIPT="mvn org.apache.maven.plugins:maven-help-plugin:2.1.1:evaluate -Dexpression=project.version | grep -Eiv 'INFO|Down|Prog'"
-ARTIFACT_NAME="${ARTIFACT_ID}-${POM_VERSION}-jar-with-dependencies.jar"
-IMAGE_NAME="mustafasisman/${ARTIFACT_ID}:${POM_VERSION}"
 
 eval ARTIFACT_ID=\$\($ARTIFACT_ID_SCRIPT\)
 echo $ARTIFACT_ID
-eval POM_VERSION=\$\(POM_VERSION_SCRIPT\)
+eval POM_VERSION=\$\($POM_VERSION_SCRIPT\)
 echo $POM_VERSION
-eval $ARTIFACT_NAME
-eval $IMAGE_NAME
+ARTIFACT_NAME=${ARTIFACT_ID}-${POM_VERSION}-jar-with-dependencies.jar
+echo $ARTIFACT_NAME
+IMAGE_NAME=mustafasisman/${ARTIFACT_ID}:${POM_VERSION}
+echo $IMAGE_NAME
