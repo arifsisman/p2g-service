e94d11ca73d2e47c39439e8c732f3ab5096a6aff
Mustafa Arif Sisman <mustafaarifsisman@gmail.com>
2020-02-29 17:30:04 +0300

    fix: Systems default TimeZone is UTC. All LocalDateTime instances zoned.

--- src/main/java/vip/yazilim/p2g/web/Play2GetherWebApplication.java
+++ src/main/java/vip/yazilim/p2g/web/Play2GetherWebApplication.java
@@ -4,9 +4,17 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
+
 @SpringBootApplication
 @EnableResourceServer
 public class Play2GetherWebApplication {
+
+    @PostConstruct
+    public void init() {
+        TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(Play2GetherWebApplication.class, args);
