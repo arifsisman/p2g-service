e1095184267832299e7818466daa0112b7b18b8a
Mustafa Arif Sisman <mustafaarifsisman@gmail.com>
2020-03-18 04:54:12 +0300

    fix: RoomService create without replacing double quotes

--- src/main/java/vip/yazilim/p2g/web/service/p2g/impl/RoomService.java
+++ src/main/java/vip/yazilim/p2g/web/service/p2g/impl/RoomService.java
@@ -78,7 +78,7 @@ public class RoomService extends ACrudServiceImpl<Room, Long> implements IRoomSe
 
     @Override
     protected Room preInsert(Room entity) {
-        if (entity.getPassword() == null) {
+        if (entity.getPassword() == null || entity.getPassword().isEmpty()) {
             entity.setPassword(null);
             entity.setPrivateFlag(false);
         } else {
@@ -245,6 +245,7 @@ public class RoomService extends ACrudServiceImpl<Room, Long> implements IRoomSe
         Room room = new Room();
         room.setOwnerId(ownerId);
         room.setName(roomName);
+        room.setPassword(roomPassword);
 
         Room createdRoom = create(room);
         roomUserService.joinRoomOwner(createdRoom.getId(), createdRoom.getOwnerId());
