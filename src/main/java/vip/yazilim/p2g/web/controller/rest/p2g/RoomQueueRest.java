79667c49bec866e36264a328265d7d7977fce376
Mustafa Arif Sisman <mustafaarifsisman@gmail.com>
2020-03-20 03:19:35 +0300

    impl: RoomQueueRest, RoomUserRest, RoomInviteRest impl: Room mappings distributed impl: Default mappings deleted

---
+++ src/main/java/vip/yazilim/p2g/web/controller/rest/p2g/RoomQueueRest.java
@@ -0,0 +1,72 @@
+package vip.yazilim.p2g.web.controller.rest.p2g;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import vip.yazilim.libs.springcore.rest.model.RestResponse;
+import vip.yazilim.p2g.web.config.annotation.HasRoomPrivilege;
+import vip.yazilim.p2g.web.config.annotation.UpdateRoomSongs;
+import vip.yazilim.p2g.web.constant.enums.Privilege;
+import vip.yazilim.p2g.web.entity.Song;
+import vip.yazilim.p2g.web.model.SearchModel;
+import vip.yazilim.p2g.web.service.p2g.ISongService;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.List;
+
+import static vip.yazilim.p2g.web.constant.Constants.API_P2G;
+
+/**
+ * @author mustafaarifsisman - 06.12.2019
+ * @contact mustafaarifsisman@gmail.com
+ */
+@RestController
+@RequestMapping(API_P2G + "/room/queue")
+public class RoomQueueRest {
+
+    @Autowired
+    private ISongService songService;
+
+    @HasRoomPrivilege(privilege = Privilege.SONG_GET)
+    @GetMapping("/{roomId}")
+    public RestResponse<List<Song>> getSongListByRoomId(HttpServletRequest request, HttpServletResponse response, @PathVariable Long roomId) {
+        return RestResponse.generateResponse(songService.getSongListByRoomId(roomId), HttpStatus.OK, request, response);
+    }
+
+    @HasRoomPrivilege(privilege = Privilege.SONG_ADD_AND_REMOVE)
+    @UpdateRoomSongs
+    @PostMapping("/{roomId}/add")
+    public RestResponse<Boolean> addSongToRoom(HttpServletRequest request, HttpServletResponse response, @PathVariable Long roomId, @RequestBody List<SearchModel> searchModelList) {
+        return RestResponse.generateResponse(songService.addSongToRoom(roomId, searchModelList), HttpStatus.OK, request, response);
+    }
+
+    @HasRoomPrivilege(privilege = Privilege.ROOM_CLEAR_QUEUE)
+    @UpdateRoomSongs
+    @DeleteMapping("/{roomId}/clear")
+    public RestResponse<Boolean> clearSongList(HttpServletRequest request, HttpServletResponse response, @PathVariable Long roomId) {
+        return RestResponse.generateResponse(songService.deleteRoomSongList(roomId), HttpStatus.OK, request, response);
+    }
+
+    @HasRoomPrivilege(privilege = Privilege.SONG_ADD_AND_REMOVE)
+    @UpdateRoomSongs
+    @DeleteMapping({"/{songId}/remove"})
+    public RestResponse<Boolean> removeSongFromRoom(HttpServletRequest request, HttpServletResponse response, @PathVariable Long songId) {
+        return RestResponse.generateResponse(songService.removeSongFromRoom(songId), HttpStatus.OK, request, response);
+    }
+
+    @HasRoomPrivilege(privilege = Privilege.SONG_VOTE)
+    @UpdateRoomSongs
+    @PutMapping("/{songId}/upvote")
+    public RestResponse<Integer> upvote(HttpServletRequest request, HttpServletResponse response, @PathVariable Long songId) {
+        return RestResponse.generateResponse(songService.upvote(songId), HttpStatus.OK, request, response);
+    }
+
+    @HasRoomPrivilege(privilege = Privilege.SONG_VOTE)
+    @UpdateRoomSongs
+    @PutMapping("/{songId}/downvote")
+    public RestResponse<Integer> downvote(HttpServletRequest request, HttpServletResponse response, @PathVariable Long songId) {
+        return RestResponse.generateResponse(songService.downvote(songId), HttpStatus.OK, request, response);
+    }
+
+}
--- src/main/java/vip/yazilim/p2g/web/controller/rest/p2g/UserFriendsRest.java
+++ src/main/java/vip/yazilim/p2g/web/controller/rest/p2g/UserFriendsRest.java
@@ -3,12 +3,9 @@ package vip.yazilim.p2g.web.controller.rest.p2g;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
-import vip.yazilim.libs.springcore.rest.ARestRead;
 import vip.yazilim.libs.springcore.rest.model.RestResponse;
-import vip.yazilim.libs.springcore.service.ICrudService;
 import vip.yazilim.p2g.web.config.annotation.HasSystemRole;
 import vip.yazilim.p2g.web.constant.enums.Role;
-import vip.yazilim.p2g.web.entity.FriendRequest;
 import vip.yazilim.p2g.web.model.FriendModel;
 import vip.yazilim.p2g.web.model.FriendRequestModel;
 import vip.yazilim.p2g.web.service.p2g.IFriendRequestService;
@@ -25,43 +22,11 @@ import static vip.yazilim.p2g.web.constant.Constants.API_P2G;
  * @contact mustafaarifsisman@gmail.com
  */
 @RestController
-@RequestMapping(API_P2G + "/friend/requests")
-public class FriendRequestRest extends ARestRead<FriendRequest, Long> {
+@RequestMapping(API_P2G + "/user/friends")
+public class UserFriendsRest {
 
     @Autowired
     private IFriendRequestService friendRequestService;
-
-    @Override
-    protected ICrudService<FriendRequest, Long> getService() {
-        return friendRequestService;
-    }
-
-    @Override
-    protected Class<FriendRequest> getClassOfEntity() {
-        return FriendRequest.class;
-    }
-
-    ///////////////////////////////
-    // Super class Read controllers
-    ///////////////////////////////
-
-    @Override
-    @HasSystemRole(role = Role.P2G_USER)
-    @GetMapping({"/{id}"})
-    public RestResponse<FriendRequest> getById(HttpServletRequest request, HttpServletResponse response, @PathVariable Long id) {
-        return super.getById(request, response, id);
-    }
-
-    @Override
-    @HasSystemRole(role = Role.P2G_USER)
-    @GetMapping({"/"})
-    public RestResponse<List<FriendRequest>> getAll(HttpServletRequest request, HttpServletResponse response) {
-        return super.getAll(request, response);
-    }
-
-    ///////////////////////////////
-    // Custom controllers
-    ///////////////////////////////
 
     @HasSystemRole(role = Role.P2G_USER)
     @PostMapping("/{userId}/add")
@@ -76,19 +41,19 @@ public class FriendRequestRest extends ARestRead<FriendRequest, Long> {
     }
 
     @HasSystemRole(role = Role.P2G_USER)
-    @PutMapping("/{friendRequestId}/accept")
+    @PutMapping("/requests/{friendRequestId}/accept")
     public RestResponse<Boolean> accept(HttpServletRequest request, HttpServletResponse response, @PathVariable Long friendRequestId) {
         return RestResponse.generateResponse(friendRequestService.acceptFriendRequest(friendRequestId), HttpStatus.OK, request, response);
     }
 
     @HasSystemRole(role = Role.P2G_USER)
-    @PutMapping("/{friendRequestId}/reject")
+    @PutMapping("/requests/{friendRequestId}/reject")
     public RestResponse<Boolean> reject(HttpServletRequest request, HttpServletResponse response, @PathVariable Long friendRequestId) {
         return RestResponse.generateResponse(friendRequestService.rejectFriendRequest(friendRequestId), HttpStatus.OK, request, response);
     }
 
     @HasSystemRole(role = Role.P2G_USER)
-    @PutMapping("/{friendRequestId}/ignore")
+    @PutMapping("/requests/{friendRequestId}/ignore")
     public RestResponse<Boolean> ignore(HttpServletRequest request, HttpServletResponse response, @PathVariable Long friendRequestId) {
         return RestResponse.generateResponse(friendRequestService.ignoreFriendRequest(friendRequestId), HttpStatus.OK, request, response);
     }
@@ -100,31 +65,19 @@ public class FriendRequestRest extends ARestRead<FriendRequest, Long> {
     }
 
     @HasSystemRole(role = Role.P2G_USER)
-    @GetMapping({"/{userId}/model"})
-    public RestResponse<List<FriendRequestModel>> getFriendRequestModel(HttpServletRequest request, HttpServletResponse response, @PathVariable String userId) {
-        return RestResponse.generateResponse(friendRequestService.getFriendRequestModelByReceiverId(userId), HttpStatus.OK, request, response);
-    }
-
-    @HasSystemRole(role = Role.P2G_USER)
-    @GetMapping({"/friends"})
+    @GetMapping({"/me"})
     public RestResponse<List<FriendModel>> getFriends(HttpServletRequest request, HttpServletResponse response) {
         return RestResponse.generateResponse(friendRequestService.getFriendsByUserId(SecurityHelper.getUserId()), HttpStatus.OK, request, response);
     }
 
     @HasSystemRole(role = Role.P2G_USER)
-    @GetMapping({"/friends/counts"})
+    @GetMapping({"/counts"})
     public RestResponse<Integer> getFriendsCounts(HttpServletRequest request, HttpServletResponse response) {
         return RestResponse.generateResponse(friendRequestService.getFriendsCountByUserId(SecurityHelper.getUserId()), HttpStatus.OK, request, response);
     }
 
     @HasSystemRole(role = Role.P2G_USER)
-    @GetMapping({"/{userId}/friends"})
-    public RestResponse<List<FriendModel>> getFriends(HttpServletRequest request, HttpServletResponse response, @PathVariable String userId) {
-        return RestResponse.generateResponse(friendRequestService.getFriendsByUserId(userId), HttpStatus.OK, request, response);
-    }
-
-    @HasSystemRole(role = Role.P2G_USER)
-    @GetMapping({"/{userId}/friends/counts"})
+    @GetMapping({"/{userId}/counts"})
     public RestResponse<Integer> getFriendsCounts(HttpServletRequest request, HttpServletResponse response, @PathVariable String userId) {
         return RestResponse.generateResponse(friendRequestService.getFriendsCountByUserId(userId), HttpStatus.OK, request, response);
     }
