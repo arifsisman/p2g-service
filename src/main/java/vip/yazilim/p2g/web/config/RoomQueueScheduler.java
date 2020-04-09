3b3fa815b41fd3cbd34503e3caea7120f9f3abc7
Mustafa Arif Sisman <mustafaarifsisman@gmail.com>
2020-04-09 23:15:30 +0300

    impl(WIP): RoomQueueScheduler

---
+++ src/main/java/vip/yazilim/p2g/web/config/RoomQueueScheduler.java
@@ -0,0 +1,64 @@
+package vip.yazilim.p2g.web.config;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+import vip.yazilim.p2g.web.controller.WebSocketController;
+import vip.yazilim.p2g.web.entity.Room;
+import vip.yazilim.p2g.web.entity.Song;
+import vip.yazilim.p2g.web.service.p2g.IRoomService;
+import vip.yazilim.p2g.web.service.p2g.ISongService;
+import vip.yazilim.p2g.web.service.spotify.impl.SpotifyPlayerService;
+import vip.yazilim.p2g.web.util.TimeHelper;
+
+import java.time.temporal.ChronoUnit;
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * @author mustafaarifsisman - 09.04.2020
+ * @contact mustafaarifsisman@gmail.com
+ */
+@EnableScheduling
+@Component
+@Transactional
+public class RoomQueueScheduler {
+
+    private Logger LOGGER = LoggerFactory.getLogger(RoomQueueScheduler.class);
+
+    @Autowired
+    private IRoomService roomService;
+
+    @Autowired
+    private ISongService songService;
+
+    @Autowired
+    private SpotifyPlayerService spotifyPlayerService;
+
+    @Autowired
+    private WebSocketController webSocketController;
+
+    @Scheduled(fixedRate = 1000)
+    public void checkRoomSongFinished() {
+        List<Room> roomList = roomService.getAll();
+
+        for (Room room : roomList) {
+            Long roomId = room.getId();
+            Optional<Song> playingOpt = songService.getPlayingSong(roomId);
+            Optional<Song> nextOpt = songService.getNextSong(roomId);
+            if (playingOpt.isPresent() && nextOpt.isPresent() && isSongFinished(playingOpt.get())) {
+                LOGGER.info("Song[{}] finished on Room[{}]. The next song will be played.", playingOpt.get().getSongId(), roomId);
+                spotifyPlayerService.roomNext(roomId);
+                webSocketController.sendToRoom("songs", roomId, songService.getSongListByRoomId(roomId));
+            }
+        }
+    }
+
+    private boolean isSongFinished(Song song) {
+        return song.getDurationMs() - (ChronoUnit.MILLIS.between(song.getPlayingTime(), TimeHelper.getLocalDateTimeNow()) + song.getCurrentMs()) < 1000;
+    }
+}
